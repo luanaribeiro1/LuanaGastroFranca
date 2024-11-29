ddc9fb0733423865d2dea6c32f7ad1c91d905d70
Yuri²² <00001120695089sp@al.educacao.sp.gov.br>
2024-11-29 13:14:35 -0300

    acabei

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Gastronomia Geral',
+    'Como é composta uma refeição tradicional completa na França?',
+    'Uma refeição normal completa consiste de aperitivo, entrada, prato principal, queijo, sobremesa e digestivo.'
+)
+
+criaCartao(
+    'Cultura Francesa',
+    'O que caracteriza a culinária francesa?',
+    'A culinária francesa é conhecida pela qualidade, variedade regional e equilíbrio dos sabores.'
+)
+
+criaCartao(
+    'Gastronomia Francesa',
+    'Qual é a estrutura de um típico jantar francês?',
+    'Uma refeição típica francesa é composta por entrada, prato principal, acompanhamento, queijo, sobremesa e digestivo.'
+)
+
+criaCartao(
+    'Receitas Francesas',
+    'Quais são os ingredientes principais de um ratatouille?',
+    'Berinjela, abobrinha, pimentão, cebola, tomate e alho, geralmente temperados com ervas como tomilho, alecrim ou manjericão. Eles são cozidos lentamente criando um prato cheio de sabores!'
+)
+
+criaCartao(
+    'Bebidas Francesas',
+    'Quais vinhos são frequentemente harmonizados com pratos da culinária francesa?',
+    'Vinhos tintos, como Bordeaux ou Bourgogne, são harmonizados com carnes, enquanto vinhos brancos, como Chablis ou Sancerre, acompanham frutos do mar e queijos macios.'
+)
